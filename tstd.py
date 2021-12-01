e252fe704d0cd2d0a11554349c7aaa4eddb11c16
rajatsoni98 <92118056+rajatsoni98@users.noreply.github.com>
2021-12-01 11:43:39 +0530

    Add files via upload
    new

---
+++ tstd.py
@@ -0,0 +1,12 @@
+def gst(a,b,c):
+    if (a>c) and (a>b):
+        print(a)
+    elif (b>a) and (b>c):
+        print(b)
+    else:
+        print(c)
+
+gst(3,4,5)
+gst(45,65,36)
+gst(6626265659662,51746278265726,32786276267267)
+gst(1,2,3)
